0093c803e15edc840e1d942e8e2dc961c77e5352
pavan <pavan.patchikarla@yahoo.com>
2022-04-20 17:47:08 +0530

    new

---
+++ app.js
@@ -0,0 +1,15 @@
+new Splide( '.splide' ).mount();
+
+
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    drag   : 'free',
+    perPage: 3,
+    } );
+    
+    splide.mount();
+
+
+    function acob(){
+        alert("THANK YOU FOR CONTACTING US WE WILL REACH YOU SOON!!")
+    }
